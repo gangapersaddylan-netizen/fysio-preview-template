3a9646aa6c4076726cf92e8b7f1484d944616e48
gangapersaddylan-netizen <gangapersaddylan@gmail.com>
2026-08-07 13:07:08 +0200

    add components/ui/card.tsx

---
+++ components/ui/card.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function Card({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "rounded-[18px] border border-line bg-card text-card-foreground",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function CardHeader({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-6", className)} {...props} />;
+}
+
+export function CardContent({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-6 pt-0", className)} {...props} />;
+}
