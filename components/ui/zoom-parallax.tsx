22ac08eb877b3022fce35fbe72f9c5c4dff36446
gangapersaddylan-netizen <gangapersaddylan@gmail.com>
2026-08-08 18:00:08 +0200

    template: portret-tegels in team-parallax

--- components/ui/zoom-parallax.tsx
+++ components/ui/zoom-parallax.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 
 interface ParallaxImage {
   src: string;
+  portret?: boolean;
   alt?: string;
 }
 
@@ -31,7 +32,7 @@ export function ZoomParallax({ images }: ZoomParallaxProps) {
   return (
     <div ref={container} className="relative h-[300vh]">
       <div className="sticky top-0 h-screen overflow-hidden">
-        {images.map(({ src, alt }, index) => {
+        {images.map(({ src, alt, portret }, index) => {
           const scale = scales[index % scales.length];
 
           return (
@@ -40,7 +41,7 @@ export function ZoomParallax({ images }: ZoomParallaxProps) {
               style={{ scale }}
               className={`absolute top-0 flex h-full w-full items-center justify-center ${index === 1 ? "[&>div]:!-top-[30vh] [&>div]:!left-[5vw] [&>div]:!h-[30vh] [&>div]:!w-[35vw]" : ""} ${index === 2 ? "[&>div]:!-top-[10vh] [&>div]:!-left-[25vw] [&>div]:!h-[45vh] [&>div]:!w-[20vw]" : ""} ${index === 3 ? "[&>div]:!left-[27.5vw] [&>div]:!h-[25vh] [&>div]:!w-[25vw]" : ""} ${index === 4 ? "[&>div]:!top-[27.5vh] [&>div]:!left-[5vw] [&>div]:!h-[25vh] [&>div]:!w-[20vw]" : ""} ${index === 5 ? "[&>div]:!top-[27.5vh] [&>div]:!-left-[22.5vw] [&>div]:!h-[25vh] [&>div]:!w-[30vw]" : ""} ${index === 6 ? "[&>div]:!top-[22.5vh] [&>div]:!left-[25vw] [&>div]:!h-[15vh] [&>div]:!w-[15vw]" : ""} `}
             >
-              <div className="relative h-[25vh] w-[25vw] overflow-hidden rounded-[16px] border border-white/10 shadow-2xl">
+              <div className={`relative overflow-hidden rounded-[16px] border border-white/10 shadow-2xl ${portret ? "h-[34vh] w-[13vw] min-w-[170px]" : "h-[25vh] w-[25vw]"}`}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src={src}
